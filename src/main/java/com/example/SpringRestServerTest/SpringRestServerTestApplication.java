e7a22b35bcc1559cfd0a0afa08e65b08d8a82226
Bastien <bchevriaut@gmail.com>
2020-07-27 15:03:51 +0200

    restarting

---
+++ src/main/java/com/example/SpringRestServerTest/SpringRestServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringRestServerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestServerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestServerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringRestServerTest/SpringRestServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringRestServerTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestServerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
